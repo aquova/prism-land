e29c278f4bf99cb6017b7cdcc54eeed2001df505
abricker <abricker@wustl.edu>
2018-03-04 17:23:40 -0600

    Modified level

---
+++ testUnityProject/Assets/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	private Rigidbody rb;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+
+	private void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		movement = Camera.main.transform.TransformDirection (movement);
+		movement.y = 0.0f;
+		rb.AddForce (movement * speed);
+	}
+}
