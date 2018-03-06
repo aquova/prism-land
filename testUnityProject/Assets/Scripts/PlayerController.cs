9474499801f3740b9dce48a72424418b682f5f56
Jack Robards <jrobards2@gmail.com>
2018-03-06 01:09:43 -0600

    Organized Assets Directory. Added Goal, basic UI, goal screen, score pickup item, and an object that causes you to be bounced off of it when touched.

---
+++ testUnityProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	private Rigidbody rb;
+    private Vector3 startingPositon;
+    private bool canMove;
+	public float speed;
+    public float Showtime = 0f;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+        startingPositon = transform.position;
+        canMove = true;
+	}
+
+	private void FixedUpdate()
+	{
+        if (canMove) {
+            float moveHorizontal = Input.GetAxis ("Horizontal");
+            float moveVertical = Input.GetAxis ("Vertical");
+
+            Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+            movement = Camera.main.transform.TransformDirection (movement);
+            movement.y = 0.0f;
+            rb.AddForce (movement * speed);
+
+            if (Showtime > 0f) {
+                Showtime = Showtime - (Time.deltaTime);
+            } else {
+                speed = 15f;
+            }
+        }
+	}
+
+    void OnTriggerEnter(Collider other) 
+    {
+        if (other.gameObject.CompareTag ("Pick Up"))
+        {
+            other.gameObject.SetActive (false);
+            speed = 40f;
+            Showtime = 3f;
+        }
+
+        if (other.gameObject.CompareTag ("Bounce"))
+        {
+            Debug.Log ("trampoline");
+            GetComponent<Rigidbody>().AddForce(Vector3.up * 1000f);
+        }
+
+    }
+
+    public void Stop() {
+        gameObject.GetComponent <Rigidbody> ().velocity = Vector3.zero;
+        gameObject.GetComponent <Rigidbody> ().angularVelocity = Vector3.zero;
+    }
+
+    public void EnableMovement() {
+        canMove = true;
+    }
+
+    public void DisableMovement() {
+        canMove = false;
+    }
+
+    public void ResetPosition() {
+        Stop ();
+        transform.position = startingPositon;
+    }
+}
---
+++ testUnityProject/Assets/Scripts/rollingMovement.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rollingMovement : MonoBehaviour {
+
+	private float speed = 15f;
+	public float Showtime = 0f;
+
+	private Rigidbody rb;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+
+		if (Showtime > 0f)
+		{
+			Showtime = Showtime - (Time.deltaTime);
+		}
+		else {
+			speed = 15f;
+		}
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag ("Pick Up"))
+		{
+            print ("here");
+			other.gameObject.SetActive (false);
+			speed = 40f;
+			Showtime = 3f;
+		}
+
+		if (other.gameObject.CompareTag ("Bounce"))
+		{
+			Debug.Log ("trampoline");
+			GetComponent<Rigidbody>().AddForce(Vector3.up * 1000f);
+		}
+
+	}
+}
